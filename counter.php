70940eb2c2437359a0a903f2bfc5d685662b6968
Jekwkxf <123891186+Jekwkxf@users.noreply.github.com>
2024-09-02 19:55:11 -0400

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,21 +1,13 @@
 <?php
-// File to store the view count
 $counterFile = 'views.txt';
 
-// Check if the file exists, if not, create it with a default count of 0
 if (!file_exists($counterFile)) {
     file_put_contents($counterFile, '0');
 }
 
-// Read the current view count
 $views = file_get_contents($counterFile);
-
-// Increment the view count
 $views++;
-
-// Write the updated view count back to the file
 file_put_contents($counterFile, $views);
 
-// Output the view count
 echo $views;
 ?>
