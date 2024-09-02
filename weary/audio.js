756f7b00afdfeb9a99a031e36fa4db2951816480
Jekwkxf <123891186+Jekwkxf@users.noreply.github.com>
2024-09-02 01:52:05 -0400

    Update audio.js

--- weary/audio.js
+++ weary/audio.js
@@ -40,8 +40,3 @@ audioPlayer.play();
 
 // Initial update of current song info
 updateCurrentSongInfo();
-
-<audio controls>
-    <source src="path/to/your/audio.mp3" type="audio/mpeg">
-    Your browser does not support the audio element.
-</audio>
